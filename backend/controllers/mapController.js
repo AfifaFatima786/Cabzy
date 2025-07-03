8481f6f6a29a1e804d70db5389cadf3f95b724eb
Afifa Fatima <afifarizvi7@gmail.com>
2025-07-03 18:27:46 +0530

    Integrated Google Maps Api

--- backend/controllers/mapController.js
+++ backend/controllers/mapController.js
@@ -48,7 +48,7 @@ module.exports.getSuggestions = async (req, res) => {
 
     try{
 
-        const errors=validationResult(req)
+        const errors=validationResult(req);
         if(!errors.isEmpty()){
             return res.status(400).json({ errors: errors.array() });
         }
